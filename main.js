97f395da88a7535f9b109dc85f2aa7cf0dc7f276
kwestein <kirsten_westeinde@hotmail.com>
2013-02-15 17:08:19 +0700

    Update main.js
    Remove unnecessary comments

--- main.js
+++ main.js
@@ -1,40 +1,37 @@
-var fs = require("fs"), //file system
+var fs = require("fs"), 
     express = require("express"),
     server = express(),
     port = process.env.PORT || 3000,
     host = process.env.IP || "127.0.0.1",
     resourceJsonFile = "contacts.json",
     resourcePath = "/contacts",
-    cors = function (request, response, next) { //cross origin resource sharing
-        //Response : the thing to send back to the client/ browser
-        //Request: http request
-        response.header("Access-Control-Allow-Origin", "*"); // *: can accept data from anywhere
-        response.header("Access-Control-Allow-Methods", "GET, PUT, OPTIONS"); //get = read access, put = write access
+    cors = function (request, response, next) {
+        response.header("Access-Control-Allow-Origin", "*"); 
+        response.header("Access-Control-Allow-Methods", "GET, PUT, OPTIONS"); 
         response.header("Access-Control-Allow-Headers", "Content-Type");
-        next(); //give back control to function that called us (kind of like return statement but multiple threads - listeners)
+        next(); 
     };
 
-//Loadng resource asynchronously. We are just assuming it has loaded by the time we need it, but are not checking/ error handling
 fs.readFile(resourceJsonFile, function (error, resourceJson) {
     server.set("resource", JSON.parse(resourceJson));
 });
 
-server.use(express.bodyParser()); //Allows us to access other domains, allows them to communicate with us
+server.use(express.bodyParser()); 
 server.use(cors);
 
 server.options(resourcePath, function (request, response) {
-    response.send(200); //http status code 200 = ok
+    response.send(200); 
 });
 
-server.get(resourcePath, function (request, response) { //(if someone gives a get request from the resource path) Implicitly returns response object
+server.get(resourcePath, function (request, response) { 
     response.json(server.get("resource"));
 });
 
-server.put(resourcePath, function (request, response) { //If someone gives a put request at the resource path
+server.put(resourcePath, function (request, response) { 
     var resource = request.body;
     server.set("resource", resource);
     fs.writeFile(resourceJsonFile, JSON.stringify(resource));
-    response.send(204); //204 – written correctly
+    response.send(204); 
 });
 
 server.listen(port);
